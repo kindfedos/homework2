8c1a15e8247bbd7c7f339c33d75b2fc1991e53b7
Fedor <kurnosenkovf@mail.ru>
2022-10-19 15:42:41 +0400

    10

---
+++ 10/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.Write("Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+ 
+for (int i = 1; i < 3 && (num <= 100 | num > 999); i++)
+{
+Console.Write( " Это не трёхзначное число! ");
+num = Convert.ToInt32(Console.ReadLine());
+}
+
+num = num / 10 % 10;
+Console.WriteLine($"Вторая цифра введённого числа: {num}");
