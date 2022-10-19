facf8cb7c4f7afb6bf6574bd6ff2c3edd031a2b6
Fedor <kurnosenkovf@mail.ru>
2022-10-19 21:10:51 +0400

    13

---
+++ 13/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num <= 100)
+{
+    Console.WriteLine("Третье цифры нет");
+    return;
+}
+int thirdValue = FindThirdValue(num);
+Console.WriteLine($"третья цифра в числе - {thirdValue}");
+
+
+int FindThirdValue( int number)
+{
+    int third = number;
+    while((number/100) > 0)
+    {
+        third = number % 10;
+        number = number / 10;
+        }
+        return third;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
